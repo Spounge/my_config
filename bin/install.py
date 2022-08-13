9517c1536774d4a761bcff51a0d8ae37b533fd85
Nelson ANGELIQUE <nelson.angelique@epitech.eu>
2022-08-13 18:32:58 +0200

    Now using ansible

--- bin/install.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3.8
-
-import install_neovim_config
-import install_tmux_config
--- bin/uninstall.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3.8
-
-import uninstall_neovim_config
-import uninstall_tmux_config
